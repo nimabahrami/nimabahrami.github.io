a4dfee2fa1335d3b2d8d6dbd4c02636430a53a63
nimabahrami <nimabhziphone5@gmail.com>
2021-03-05 22:40:49 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
 const selectElement = (s) => document.querySelector(s);
 
 selectElement('.open').addEventListener('click', () => {
-    selectElement('.nav-list').classList.add('active').remove('clicked');
+    selectElement('.nav-list').classList.add('active');
 });
 
 selectElement('.close').addEventListener('click', () => {
-    selectElement('.nav-list').classList.remove('active').add('clicked');
+    selectElement('.nav-list').classList.remove('active');
 });
 
 selectElement('.nav-items').addEventListener('click', () => {
-    selectElement('.nav-list').classList.remove('active').add('clicked');
+    selectElement('.nav-list').classList.remove('active')
 });
 
 selectElement('.nav-items-2').addEventListener('click', () => {
-    selectElement('.nav-list').classList.remove('active').add('clicked');
+    selectElement('.nav-list').classList.remove('active')
 });
 
+window.onscroll = function() {
+    // We add pageYOffset for compatibility with IE.
+    if (window.scrollY >= 100 || window.pageYOffset >= 100) {
+      document.getElementsByTagName("header")[0].classList.add("header-bg");
+    } else {
+      document.getElementsByTagName("header")[0].classList.remove("header-bg");
+    }
+  };
+  
