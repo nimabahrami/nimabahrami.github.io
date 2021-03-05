befe117704d6ffbd92b49d5f208a45a2e1c3862e
nimabahrami <nimabhziphone5@gmail.com>
2021-03-05 19:11:40 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 const selectElement = (s) => document.querySelector(s);
 
 selectElement('.open').addEventListener('click', () => {
-    selectElement('.nav-list').classList.add('active');
+    selectElement('.nav-list').classList.add('active').remove('clicked');
 });
 
 selectElement('.close').addEventListener('click', () => {
-    selectElement('.nav-list').classList.remove('active');
+    selectElement('.nav-list').classList.remove('active').add('clicked');
 });
+
+selectElement('.nav-items').addEventListener('click', () => {
+    selectElement('.nav-list').classList.remove('active').add('clicked');
+});
+
+selectElement('.nav-items-2').addEventListener('click', () => {
+    selectElement('.nav-list').classList.remove('active').add('clicked');
+});
+
