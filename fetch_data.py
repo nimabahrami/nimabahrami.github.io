d8e2c1167f5e119d190df9c9c5aadd2fcfdb9356
nimabahrami <nimabhziphone5@gmail.com>
2025-04-13 20:08:31 +0200

    Update fetch_data.py

--- fetch_data.py
+++ fetch_data.py
@@ -3,15 +3,17 @@ import os
 import json
 
 api_key = os.getenv("RESTDB_API_KEY")
-url = "https://yourproject.restdb.io/rest/yourcollection"
+url = "https://myen3rgyin7estments-3498.restdb.io/rest/stocks"
 
 headers = {
+    'content-type': "application/json",
     "x-apikey": api_key,
-    "Content-Type": "application/json"
-}
+    'cache-control': "no-cache"
+    }
 
 response = requests.get(url, headers=headers)
 data = response.json()
 
 with open("public/data.json", "w") as f:
     json.dump(data, f, indent=2)
+
