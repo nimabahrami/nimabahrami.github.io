3487838c51bbf9e6d81ce56599d2233b6f573824
nimabahrami <nimabhziphone5@gmail.com>
2025-04-14 00:30:31 +0200

    Update fetch_data.py

--- fetch_data.py
+++ fetch_data.py
@@ -3,8 +3,6 @@ import os
 import json
 
 token = os.getenv("SHEETDB_AUTH")
-print("Got token?" if token else "No token found")
-print("First 4 chars:", token[:4] if token else "N/A")
 
 url = "https://sheetdb.io/api/v1/59mfw9fgs9c7x"
 headers = {"Authorization": f"Bearer {token}",
